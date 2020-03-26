827d246cd9109de7d45c659c6b4f39e0e8d4b880
Divyaraj Bakrola <divyaraj@Divyarajs-MacBook-Pro.local>
2020-03-26 16:59:12 -0700

    version 1.1  Alpha Test

--- php/contactform.php
+++ php/contactform.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
+$pp->sendEmailTo('bakrola.divyaraj@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
