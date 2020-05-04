435668a87caa2074cfb898b97dd8c259975bbdf6
Divyaraj Bakrola <divyaraj@Divyarajs-MacBook-Pro.local>
2020-05-03 20:43:56 -0700

    Contact form Bug Fix1

--- php/contactform.php
+++ php/contactform.php
@@ -21,6 +21,6 @@ $validator->field('message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('bakrola.divyaraj@gmail.com'); // ← Your email here
+$pp->sendEmailTo('dbakrola@mail.sfsu.edu'); // ← Your email here
 
 echo $pp->process($_POST);
